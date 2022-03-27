b25393e0139e86b81685d308b61915ef21970e29
Ali Nauman <ali.nauman@arbisoft.com>
2022-03-27 17:08:17 +0500

    feat: add more test for user model

--- src/models/store_user.spec.ts
+++ src/models/store_user.spec.ts
@@ -38,6 +38,16 @@ describe('Test the user model', () => {
     expect(result.last_name).toBe(user.last_name);
   });
 
+  it('Expects authenticate with incorrect password to fail auth', async () => {
+    const result = await userStore.authenticate({...user, password: '123'});
+
+    if (typeof result !== 'string') {
+      throw Error('Unexpected type!')
+    }
+
+    expect(result).toBe('Incorrect password!');
+  });
+
   it('Expects delete with id 1 to contain our product', async () => {
     const result = await userStore.delete('1');
     expect(result.first_name).toBe(user.first_name);
