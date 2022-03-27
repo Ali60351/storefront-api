4d427546e5a3cd31e6a02986f4deb5c7e52e32c1
Ali Nauman <ali.nauman@arbisoft.com>
2022-03-27 14:28:53 +0500

    feat: setup env for testing

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,20 @@
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
 
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
 
-const client = new Pool({
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+const {
+    POSTGRES_HOST,
+    POSTGRES_DB,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+} = process.env;
+
+const client: Pool = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
 });
 
 export default client;
